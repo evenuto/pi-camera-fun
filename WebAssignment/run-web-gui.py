3da2dde614026d5655445b31590a896c0c2fc2ca
N03459074 <venutoe1@hawkmail.newpaltz.edu>
2018-03-08 23:52:21 -0500

    parameter request can be made

--- WebAssignment/run-web-gui.py
+++ WebAssignment/run-web-gui.py
@@ -1,25 +1,35 @@
 #!/usr/bin/python
 import sqlite3 as sql
-import os
-import time
-import datetime
+from datetime import datetime
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def main():
     return render_template('home.html')
 
-@app.route('/list')
-def list():
-    con = sql.connect("/home/pi/ELSpring2018/WebAssignment/myTemps.db")
-    con.row_factory = sql.Row
+@app.route('/list', methods=['POST'])
+def display():
+    if request.method == 'POST':
+        start = request.form['start']
+        end = request.form['end']
+        print start
+        startObj = datetime.strptime(start, '%Y-%m-%d')
+        endObj = datetime.strptime(end, '%Y-%m-%d')
 
-    cur = con.cursor()
-    cur.execute("SELECT * FROM myTemps")
+        s = startObj.strftime('%m/%d/%Y')
+        e = endObj.strftime('%m/%d/%Y')
 
-    rows = cur.fetchall();
-    return render_template("list.html",rows = rows)
+        con = sql.connect("/home/pi/ELSpring2018/WebAssignment/myTemps.db")
+        cur = con.cursor()
+        con.row_factory = sql.Row
+
+        cur.execute('''SELECT * FROM myTemps WHERE currentTime BETWEEN ? AND ?''', (s, e,))
+
+        rows = cur.fetchall();
+        return render_template('list.html',rows = rows)
+    con.close()
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=8080, debug=True)
+

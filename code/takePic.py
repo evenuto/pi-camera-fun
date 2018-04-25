5f3e8084ee58c80c003bd3a598d60c92f40dd6ca
N03459074 <venutoe1@hawkmail.newpaltz.edu>
2018-04-25 13:32:18 -0400

    trying to get different directions working

--- code/takePic.py
+++ code/takePic.py
@@ -3,23 +3,34 @@ import sqlite3
 import time
 import datetime
 from flask import Flask, render_template, Response, url_for, request
-import picamera 
+import picamera
 import RPi.GPIO as GPIO
 app = Flask(__name__)
 
-@app.route("/moveServo")
-def moveServo():
+@app.route("/<direction>")
+def moveServo(direction):
     GPIO.setmode(GPIO.BOARD)
-    panPin = 11
-    GPIO.setup(panPin,GPIO.OUT)
-    pwm=GPIO.PWM(panPin,50)
+    if direction == "right":
+        currentPin = int(11)
+        myAngle = int(180)
+    if direction == "left":
+        currentPin = int(11)
+        myAngle = int(0)
+    if direction == "down":
+        currentPin = int(7)
+        myAngle = int(180)
+    if direction == "up":
+        currentPin = int(7)
+        myAngle = int(0)
+
+    GPIO.setup(currentPin,GPIO.OUT)
+    pwm=GPIO.PWM(currentPin,50)
     pwm.start(7)
-    for i in range(0,180):
-      DC=1./18.*(i)+2
-      pwm.ChangeDutyCycle(DC)
-    for i in range(0,180):
-      DC=1/18.*i+2
-      pwm.ChangeDutyCycle(DC)
+
+    # formula to determine proper duty cycle
+    dc = 1./20.*(myAngle)+2
+    pwm.ChangeDutyCycle(dc)
+    #time.sleep(0.3)
     pwm.stop()
     GPIO.cleanup()
     return render_template('test.html')
